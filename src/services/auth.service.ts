6202c80b16f4a40dfdba8ce8ad886699ef043ee3
achhayapathak <achhayapathak11@gmail.com>
2025-03-14 17:52:01 +0530

    v2.2.0: Added login endpoint and fixed a bug

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken";
 import prisma from "../config/db";
 import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
-import { hashValue } from "../utils/bcrypt";
+import { compareHash, hashValue } from "../utils/bcrypt";
 import { oneYearFromNow, thirtyDaysFromNow } from "../utils/date";
 import appAssert from "../utils/appAssert";
-import { CONFLICT } from "../constants/http";
+import { CONFLICT, UNAUTHORIZED } from "../constants/http";
 
 export type createAccountParams = {
   email: string;
   password: string;
-  userAgent: string;
+  userAgent?: string;
 };
 
 export const createAccount = async (data: createAccountParams) => {
@@ -19,11 +19,6 @@ export const createAccount = async (data: createAccountParams) => {
       email: data.email,
     },
   });
-
-  // if (existingUser) {
-  //   throw new Error("User already exists");
-  // }
-
   appAssert(!existingUser, CONFLICT, "User already exists");
 
   // hash password
@@ -60,7 +55,7 @@ export const createAccount = async (data: createAccountParams) => {
   // sign access token and refresh token
   const refreshToken = jwt.sign({ sessionId: session.id }, JWT_REFRESH_SECRET, {
     expiresIn: "30d",
-    audience: user.id,
+    audience: ["user"],
   });
 
   const accessToken = jwt.sign(
@@ -71,13 +66,71 @@ export const createAccount = async (data: createAccountParams) => {
     JWT_SECRET,
     {
       expiresIn: "15m",
-      audience: user.id,
+      audience: ["user"],
     }
   );
 
   // return user and tokens
   return {
-    user: { user_created: user.email },
+    user,
+    accessToken,
+    refreshToken,
+  };
+};
+
+export const loginUser = async (data: createAccountParams) => {
+  // get user by email
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  // verify user exists
+  appAssert(user, UNAUTHORIZED, "Invalid email or password");
+
+  // verify password
+  if (data.password && user.password) {
+    const isValid = await compareHash(data.password, user.password);
+    appAssert(isValid, UNAUTHORIZED, "Invalid email or password");
+  }
+
+  // create session
+  const session = await prisma.session.create({
+    data: {
+      userId: user.id,
+      userAgent: data.userAgent,
+      expiresAt: thirtyDaysFromNow(),
+    },
+  });
+
+  // sign access token and refresh token
+  const refreshToken = jwt.sign(
+    {
+      sessionId: session.id,
+    },
+    JWT_REFRESH_SECRET,
+    {
+      expiresIn: "30d",
+      audience: ["user"],
+    }
+  );
+
+  const accessToken = jwt.sign(
+    {
+      sessionId: session.id,
+      userId: user.id,
+    },
+    JWT_SECRET,
+    {
+      expiresIn: "15m",
+      audience: ["user"],
+    }
+  );
+
+  // return user and tokens
+  return {
+    user,
     accessToken,
     refreshToken,
   };
