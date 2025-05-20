cee3172af25fbd48cd75e71398ecb2eaaac41ec7
achhayapathak <achhayapathak11@gmail.com>
2025-05-20 12:15:10 +0530

    v3.13.0-feat: Added Rate Limitter

--- src/constants/appErrorCode.ts
+++ src/constants/appErrorCode.ts
@@ -2,6 +2,7 @@ const enum AppErrorCode {
   InvalidAccessToken = "InvalidAccessToken",
   BadRequest = "BadRequest",
   Unauthorized = "Unauthorized",
+  TooManyRequests = "TooManyRequests",
 }
 
 export default AppErrorCode;
